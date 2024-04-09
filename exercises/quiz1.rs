5faa6d6aa0f617b1dfbe81f911ab2ad08eb2dd2b
Super User <root@mosheng>
2024-04-09 15:58:28 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples (a:i32)->i32{
+    if a>40 {
+        a
+    }else {
+        a*2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
