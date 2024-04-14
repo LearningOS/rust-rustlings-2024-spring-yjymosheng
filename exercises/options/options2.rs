bc1951ab497beed1ad94570cc208a3f4774b938c
yjymosheng <yjymosheng@gmail.com>
2024-04-15 03:38:37 +0800

    update

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,40 +3,40 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn simple_option() {
-        let target = "rustlings";
-        let optional_target = Some(target);
+        #[test]
+        fn simple_option() {
+                let target = "rustlings";
+                let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
-        }
-    }
-
-    #[test]
-    fn layered_option() {
-        let range = 10;
-        let mut optional_integers: Vec<Option<i8>> = vec![None];
-
-        for i in 1..(range + 1) {
-            optional_integers.push(Some(i));
+                // TODO: Make this an if let statement whose value is "Some" type
+                if let Some(word) = optional_target {
+                        assert_eq!(word, target);
+                }
         }
 
-        let mut cursor = range;
+        #[test]
+        fn layered_option() {
+                let range = 10;
+                let mut optional_integers: Vec<Option<i8>> = vec![None];
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
-            cursor -= 1;
+                for i in 1..(range + 1) {
+                        optional_integers.push(Some(i));
+                }
+
+                let mut cursor = range;
+
+                // TODO: make this a while let statement - remember that vector.pop also
+                // adds another layer of Option<T>. You can stack `Option<T>`s into
+                // while let and if let.
+                while let Some(Some(integer)) = optional_integers.pop()
+                {
+                        assert_eq!(integer, cursor);
+                        cursor -= 1;
+                }
+
+                assert_eq!(cursor, 0);
         }
-
-        assert_eq!(cursor, 0);
-    }
 }
